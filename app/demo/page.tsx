82722f523b772f58048bcd546a34526e000fcfd3
Aadithya Senthil Kumaran <aadithyacps@gmail.com>
2026-04-12 12:25:49 +0530

    force dynamic render for /demo

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,6 +1,8 @@
 import Workspace from "@/components/querylens/workspace"
 import { getBootstrapPayload } from "@/lib/querylens/server/bootstrap"
 
+export const dynamic = "force-dynamic"
+
 export default async function DemoPage() {
   const bootstrap = await getBootstrapPayload()
 
